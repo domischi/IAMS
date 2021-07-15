2aa6aa3a9ec3012509a358de3e955720113967dd
Dominik Schildknecht <dominik.schildknecht@gmail.com>
2021-07-15 13:43:49 -0700

    Include an automatic check for updated git's in the push-new-docker file

--- docker/push-new-docker.sh
+++ docker/push-new-docker.sh
@@ -1,4 +1,27 @@
 #!/bin/sh
+
+# Assert that the git repo and all it's submodules are clean
+if [ -z "$(git status --untracked-files=no --porcelain)" ]; then
+    true # Equivalent of python's pass
+else
+    echo "Uncommitted changes, please first commit all changes, then run this script again... "
+    exit
+fi
+
+# Update submodules
+git submodule update --recursive --remote
+
+# Assert that the submodules are included as well
+if [ -z "$(git status --untracked-files=no --porcelain)" ]; then
+    true # Equivalent of python's pass
+else
+    echo "There have been changes in the submodules, add them to the IAMS repo, then run this script again..."
+    exit
+fi
+
+# Update remote version of IAMS (which is used to generate the docker container)
+git push
+
 # Authenticate 
 aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin $(cat ECR_LINK)
 
