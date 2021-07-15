a9356798e9ecc187cfc7ba93895ef039df353154
Dominik Schildknecht <dominik.schildknecht@gmail.com>
2021-07-15 14:20:34 -0700

    Update the AWS worker to include ANFDM as well

--- docker/worker.py
+++ docker/worker.py
@@ -1,7 +1,7 @@
 import os
 import uuid
 from IAMS.helper import get_queued_experiments, get_ith_simulation, download_queue_from_s3
-from IAMS.runners.SpringBoxRunner import run_one, RESOURCE_FOLDER_DIR
+from IAMS.runners.runner_helper import RESOURCE_FOLDER_DIR
 from datetime import date
 if __name__ == '__main__':
     index = int(os.environ.get("AWS_BATCH_JOB_ARRAY_INDEX", 0))
@@ -9,4 +9,10 @@ if __name__ == '__main__':
     queue_fname = uid+".json"
     download_queue_from_s3(uid, RESOURCE_FOLDER_DIR)
     sim = get_ith_simulation(index, queue_file_location=queue_fname)
+    if sim.get('SIMULATION_TYPE').lower() == 'Springbox'.lower():
+        from IAMS.runners.SpringBoxRunner import run_one
+    elif sim.get('SIMULATION_TYPE').lower() == 'ANFDM'.lower():
+        from IAMS.runners.ANFDMRunner import run_one
+    else:
+        raise RuneimeError(f"Unrecognized simuation_type: sim.get('SIMULATION_TYPE')...")
     run_one(sim, do_local=False, do_S3=True, folder_modifier=str(date.today())+'-'+str(index))
